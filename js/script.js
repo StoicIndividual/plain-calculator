ab2a79360077e158685a68e97a5e5c524c8f4bfa
StoicIndividual <WimVeto@protonmail.com>
2019-10-18 00:34:16 -0300

    Extra features and minor tweaks
    Added the tens, hundreds and decimal buttons along with the
    remembering of history functionality

--- js/script.js
+++ js/script.js
@@ -6,9 +6,77 @@ const startCalc = () => {
     //for buttons
     const Buttons = document.querySelectorAll('#content #calcbody .buttonsbody .numbers button');
     const Operators = document.querySelectorAll('#content #calcbody .buttonsbody .functions button');
+    //for history
+    const forwardAndBack = document.querySelectorAll('#content #calcbody .buttonsbody .history button')
+    const histArr = [];
+    let futArr = [];
+
+    forwardAndBack.forEach(time => {
+	time.addEventListener('click', function() {
+	    if(time.classList.contains('back')) {
+		const redundantFut = futArr.includes(displayS.innerHTML);
+		if(histArr.length === 0) {
+		    if (displayS.innerHTML === '') {
+		    displayP.innerHTML='';
+		    displayP.innerHTML='Output goes here...';
+		    displayS.innerHTML='';
+		    } else {
+	            futArr.push(displayS.innerHTML);
+		    displayP.innerHTML='';
+		    displayP.innerHTML='Output goes here...';
+		    displayS.innerHTML='';
+		    }
+		} else {
+		    if(displayS.innerHTML==='') {
+			const prevVal = histArr[histArr.length-1];
+			displayR.innerHTML='';
+			displayS.innerHTML='';
+			displayS.innerHTML=prevVal;
+			histArr.pop();
+			console.log(histArr);
+			console.log(futArr);
+		    } else {
+			const prevVal = histArr[histArr.length-1];
+			displayR.innerHTML='';
+			futArr.push(displayS.innerHTML);
+			displayS.innerHTML='';
+			displayS.innerHTML=prevVal;
+			histArr.pop();
+			console.log(histArr);
+			console.log(futArr);
+		    }
+		} 
+	    } else {
+		const comparison = futArr.length <= 0;
+		if(!comparison) {
+		    if (displayP.innerHTML === 'Output goes here...') {
+			const nextVal = futArr[futArr.length-1];
+			displayP.innerHTML='';
+			displayR.innerHTML='';
+			displayS.innerHTML='';
+			displayS.innerHTML=nextVal;
+			futArr.pop();
+			console.log(histArr);
+			console.log(futArr);
+		    } else {
+			const nextVal = futArr[futArr.length-1];
+			const redundantHist = histArr.includes(displayS.innerHTML);
+			displayR.innerHTML='';
+			histArr.push(displayS.innerHTML);
+			displayS.innerHTML='';
+			displayS.innerHTML=nextVal;
+			futArr.pop();
+			console.log(histArr);
+			console.log(futArr);    
+		    }
+		}
+	    }
+	});
+    });
     
     Buttons.forEach(button => {
 	button.addEventListener('click', function(){
+	    futArr = [];
 	    displayP.innerHTML='';
 	    displayR.innerHTML='';
 	    const addToDisplay = document.createTextNode(this.value);
@@ -18,15 +86,22 @@ const startCalc = () => {
     Operators.forEach(operator => {
 	operator.addEventListener('click', function() {
 	    if(operator.classList.contains("clear")) {
-		const displaySValue = displayS.innerHTML;
-		const cleared = displaySValue.slice(0, displaySValue.length - 1);
-		displayS.innerHTML = cleared;
+		if (displayS.innerHTML.length===1) {
+		    displayP.innerHTML='Output goes here...';
+		    displayS.innerHTML='';
+		} else {
+		    const displaySValue = displayS.innerHTML;
+		    const cleared = displaySValue.slice(0, displaySValue.length - 1);
+		    displayS.innerHTML = cleared;    
+		}
 	    } else if (operator.classList.contains("equal")) {
 		const displayArr=displayS.innerHTML.split(' ');
 		const displayString=displayArr.toString();
 		const displayOnDisplay= new Function('return '+displayString);
 		// or const displayOnDisplay = eval(displayString); but not recomended?
 		displayR.innerHTML=displayOnDisplay();
+		
+		histArr.push(displayS.innerHTML);
 		displayS.innerHTML='';
 
 	    } else if (operator.classList.contains("reset")) {
@@ -34,6 +109,7 @@ const startCalc = () => {
 		displayR.innerHTML='';
 		displayS.innerHTML='';   
 	    } else {
+		futArr = [];
 		displayP.innerHTML='';
 		const previousVal = document.createTextNode(displayR.innerHTML);
 		const addToDisplay = document.createTextNode(this.value);
